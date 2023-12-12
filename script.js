117a1af984ad7362200de9c7fc8a1b2165a587eb
Yousseflamrani <86467242+Yousseflamrani@users.noreply.github.com>
2023-12-12 11:28:34 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+function showAnswer() {
+    document.getElementById('answerPopup').style.display = 'block';
+}
+
+function closePopup() {
+    document.getElementById('answerPopup').style.display = 'none';
+}
+function showAnswer2() {
+    document.getElementById('answerPopup2').style.display = 'block';
+}
+
+function closePopup2() {
+    document.getElementById('answerPopup2').style.display = 'none';
+}
+function showAnswer3() {
+    document.getElementById('answerPopup3').style.display = 'block';
+}
+
+function closePopup3() {
+    document.getElementById('answerPopup3').style.display = 'none';
+}
+
+function showAnswer4() {
+    document.getElementById('answerPopup4').style.display = 'block';
+}
+
+function closePopup4() {
+    document.getElementById('answerPopup4').style.display = 'none';
+}
